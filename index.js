09da27342c384bd1bf61ba8e0a1d885c26702d52
Kyle Weisse <kyle.weisse@gmail.com>
2021-07-31 11:31:28 -0400

    changed to mongoose create

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ connect.then(() => {
 
     console.log('Connected correctly to server');
 
-    const newCampsite = new Campsite({
+    Campsite.create({
         name: 'React Lake Campground',
         description: 'reactlakecampground test inside of node-mongoose folder'
-    });
-
-    newCampsite.save()
+    })
     .then(campsite => {
         console.log(campsite);
         return Campsite.find();
